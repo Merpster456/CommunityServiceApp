d2fd6db5597139a5ef6cb7f680c2f16d6ffc185e
Merpster <ryan.quirk6@protonmail.ch>
2020-02-26 12:40:45 -0500

    FIN!!!

--- mac_start.sh
+++ mac_start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java --module-path ~/Downloads/javafx-sdk-13/lib --add-modules javafx.controls,javafx.fxml,javafx.graphics,javafx.web -jar FBLA-Coding.jar
+java --module-path ./javafx-sdk-13/lib --add-modules javafx.controls,javafx.fxml,javafx.graphics,javafx.web -jar FBLA-Coding.jar
